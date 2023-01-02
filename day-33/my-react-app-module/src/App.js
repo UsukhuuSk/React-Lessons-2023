fba9823c2ecab35d73d669ffb51578262c7bba7c
Morning <morning@admins-iMac-3.local>
2023-01-02 12:31:00 +0800

    product thingy

--- day-33/my-react-app-module/src/App.js
+++ day-33/my-react-app-module/src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./yellow.png";
+import submit from "./rose.png";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Popular Products</h1>
+        <hr/>
       </header>
+      <body>
+        <div className="overall">
+          <div className="productno1">
+          <i class="fa-solid fa-triangle"></i>
+            <img src={logo} className="App-logo" alt="logo" />
+            <div className="rightside">
+              <p className="title">Haught or Naught</p>
+              <p>High minded or absent-minded? You decide.</p>
+              <p className="submit">Submitted by:{<img src={submit} className="submit-pic"/>} </p>
+            </div>
+          </div>
+        </div>
+      </body>
     </div>
   );
 }
