2a0f82540c3d92f1deb055605b2978ac03343a6f
Morning <morning@admins-iMac-3.local>
2023-01-04 11:42:42 +0800

    React props

--- day-33/my-react-app-module/src/App.js
+++ day-33/my-react-app-module/src/App.js
@@ -2,26 +2,44 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import ProductFunc from "./productFunc";
 import products from "./Seed";
+import Product from "./product";
 
 function App() {
+  const productList = products.map((product) => {
+    return (
+      <ProductFunc
+        title={product.title}
+        productImageUrl={product.productImageUrl}
+        description={product.description}
+        submitterAvatarUrl={product.submitterAvatarUrl}
+        url={product.url}
+        id={product.id}
+        votes={product.votes}
+      />
+    );
+  });
+  const productList2 = products.map((product) => {
+    return (
+      <Product
+        title={product.title}
+        productImageUrl={product.productImageUrl}
+        description={product.description}
+        submitterAvatarUrl={product.submitterAvatarUrl}
+        url={product.url}
+        id={product.id}
+        votes={product.votes}
+      />
+    );
+  });
   return (
     <div className="App">
       <header className="App-header">
         <h1>Popular Products</h1>
         <hr />
         <div className="overall">
-          {products.map((product) => {
-    
-            return <ProductFunc 
-            title={product.title}
-            productImageUrl={product.productImageUrl}
-            description={product.description}
-            submitterAvatarUrl={product.submitterAvatarUrl}
-            url={product.url}
-            id={product.id}
-            votes={product.votes}
-            />;
-          })}
+          {productList}
+          <hr/>
+          {productList2}
         </div>
       </header>
     </div>
