013524d4e55d862e47421aaa6c56c758f9c39b34
Morning <morning@admins-iMac-3.local>
2023-01-09 10:30:00 +0800

    day-38

---
+++ day-38/src/components/Button.jsx
@@ -0,0 +1,12 @@
+export default function Button(){
+    // function handleClick(){
+    //     alert('You clicked me!');
+    // }
+    return(
+        <button onClick={() => {    
+            alert('You clicked me!')
+        }}>
+            Click me
+        </button>
+    )
+}
