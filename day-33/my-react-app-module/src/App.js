19c0c7598453543cb3c170847ea2589252235f14
Morning <morning@admins-iMac-3.local>
2023-01-10 12:31:21 +0800

    day-39

--- day-33/my-react-app-module/src/App.js
+++ day-33/my-react-app-module/src/App.js
@@ -1,11 +1,11 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
-import ProductFunc from "./productFunc";
-import products from "./Seed";
-import Product from "./product";
+import ProductFunc from "./components/productFunc";
+import products from "./data/Seed";
 
 function App() {
   const productList = products.map((product) => {
+    console.log(product);
     return (
       <ProductFunc
         title={product.title}
@@ -15,31 +15,20 @@ function App() {
         url={product.url}
         id={product.id}
         votes={product.votes}
+        stars={product.stars}
       />
     );
   });
-  const productList2 = products.map((product) => {
-    return (
-      <Product
-        title={product.title}
-        productImageUrl={product.productImageUrl}
-        description={product.description}
-        submitterAvatarUrl={product.submitterAvatarUrl}
-        url={product.url}
-        id={product.id}
-        votes={product.votes}
-      />
-    );
-  });
+
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Popular Products</h1>
+        <h1>Popular Products </h1>
         <hr />
         <div className="overall">
           {productList}
-          <hr/>
-          {productList2}
+          <hr />
+          {/* {productList2} */}
         </div>
       </header>
     </div>
