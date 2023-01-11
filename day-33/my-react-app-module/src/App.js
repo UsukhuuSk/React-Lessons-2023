5e5cca789084317a9324c39e750a5a4372d359f4
Morning <morning@admins-iMac-3.local>
2023-01-11 11:18:05 +0800

    day-40

--- day-33/my-react-app-module/src/App.js
+++ day-33/my-react-app-module/src/App.js
@@ -2,10 +2,36 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import ProductFunc from "./components/productFunc";
 import products from "./data/Seed";
+import { useState } from "react";
 
 function App() {
-  const productList = products.map((product) => {
-    console.log(product);
+  const [productList, setProductList] = useState(products);
+  function handleProductUpVote(productId) {
+    // console.log("upvoted", productId);
+    // const foundProduct = products.filter((product) => {
+    //   if (product.id == productId) {
+    //     return product;
+    //   }
+    // });
+    // console.log(foundProduct[0].votes);
+    // foundProduct[0].votes = productId[0].votes + 1;
+    // console.log(foundProduct[0].votes);
+
+    // console.log(products);
+    const newProducts = productList.map((product) => {
+      if (product.id === productId) {
+        return Object.assign({}, product, {
+          votes: product.votes + 1,
+        });
+      } else {
+        return product;
+      }
+    });
+    // console.log(newProducts);
+    setProductList(newProducts);
+  }
+  const productComponents = productList.map((product) => {
+    // console.log(product);
     return (
       <ProductFunc
         title={product.title}
@@ -15,6 +41,7 @@ function App() {
         url={product.url}
         id={product.id}
         votes={product.votes}
+        onVote={handleProductUpVote}
         stars={product.stars}
       />
     );
@@ -26,7 +53,7 @@ function App() {
         <h1>Popular Products </h1>
         <hr />
         <div className="overall">
-          {productList}
+          {productComponents}d
           <hr />
           {/* {productList2} */}
         </div>
--- day-33/my-react-app-module/src/components/productFunc.jsx
+++ day-33/my-react-app-module/src/components/productFunc.jsx
@@ -2,34 +2,27 @@ import { useState } from "react";
 import { Rating } from "react-simple-star-rating";
 
 function ProductFunc(props) {
-  console.log(props.stars)
-  const [votes, setVotes] = useState(props.votes);
-  const [stars, setStars] = useState(3)
-  function handleUpVote(props) {
-    setVotes(votes + 1);
-    console.log(props.stars)
+  console.log(props.stars);
+  const [stars, setStars] = useState(3);
+  // function handleUpVote(props) {
+  //   console.log(props.stars);
 
-    // let newProducts = [];
-    // const foundProduct = products.map((product) => {
-    //   if (product.id === props.id) {
-    //     product.votes = product.votes + 1;
-    //   }
-    //   return product;
-    // });
-    // console.log(foundProduct);
-  }
+  // let newProducts = [];
+  // const foundProduct = products.map((product) => {
+  //   if (product.id === props.id) {
+  //     product.votes = product.votes + 1;
+  //   }
+  //   return product;
+  // });
+  // console.log(foundProduct);
+  // }
 
   return (
     <div className="productno1">
       <img src={props.productImageUrl} className="App-logo" alt="logo" />
       <div className="rightside">
         <div className="upvotecount">
-          <a
-            href="#"
-            onClick={() => {
-              handleUpVote(props);
-            }}
-          >
+          <a href="#" onClick={() => props.onVote(props.id)}>
             <svg
               xmlns="http://www.w3.org/2000/svg"
               width="34"
@@ -41,7 +34,7 @@ function ProductFunc(props) {
               <path d="m7.247 4.86-4.796 5.481c-.566.647-.106 1.659.753 1.659h9.592a1 1 0 0 0 .753-1.659l-4.796-5.48a1 1 0 0 0-1.506 0z" />
             </svg>
           </a>
-          <p className="count"> {votes}</p>
+          <p className="count"> {props.votes}</p>
         </div>
         <p className="title">{props.title}</p>
         <p>{props.description}</p>
@@ -54,13 +47,13 @@ function ProductFunc(props) {
             />
           }
         </p>
-        <Rating onClick={()=> {
-          console.log('clicked')
-        }}
-        initialValue={3}
+        <Rating
+          onClick={() => {
+            console.log("clicked");
+          }}
+          initialValue={props.stars}
         />
       </div>
-     
     </div>
   );
 }
