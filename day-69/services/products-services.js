b1cabe3792b3b0a7ab034f530f6c6406389035d8
Morning <morning@admins-iMac-3.local>
2023-03-10 12:30:13 +0800

    day70smth

--- day-69/services/products-services.js
+++ day-69/services/products-services.js
@@ -1,5 +1,25 @@
 import { pool } from "../config/mysql-config.js";
 
+export async function getAllProducts() {
+  const [rows] = await pool.query(`select * from products`);
+  await Promise.all(
+    rows.map(async (row) => {
+      console.log(row);
+      const descriptions = await getDescriptions(row.id);
+      row.description = descriptions.map((d) => d.description);
+      const reviews = await getReviews(row.id);
+      row.reviews = reviews;
+      const images = await getImages(row.id);
+      row.imgUrl = images;
+      const size = await getSize(row.id);
+      row.size = size.map((m) => m.size);
+      const color = await getColors(row.id);
+      row.color = color.map((c) => c.color);
+      console.log(descriptions);
+    })
+  );
+  return rows;
+}
 export async function search(keyword) {
   const [rows] = await pool.query(
     `select * from products WHERE title LIKE '%${keyword}%'`
@@ -9,15 +29,15 @@ export async function search(keyword) {
     rows.map(async (row) => {
       console.log(row);
       const descriptions = await getDescriptions(row.id);
-      row.description = descriptions;
+      row.description = descriptions.map((d) => d.description);
       const reviews = await getReviews(row.id);
       row.reviews = reviews;
       const images = await getImages(row.id);
-      row.images = images;
+      row.imgUrl = images;
       const size = await getSize(row.id);
       row.size = size;
-      const colors = getColors(row.id);
-      row.colors = colors;
+      const color = await getColors(row.id);
+      row.color = color.map((c) => c.color);
       console.log(descriptions);
     })
   );
@@ -47,14 +67,14 @@ export async function getImages(productId) {
 }
 export async function getSize(productId) {
   const [rows] = await pool.query(
-    `select * from product_size WHERE product_id=?`,
+    `select s.size from product_size ps LEFT JOIN size s ON s.id = ps.size_id  WHERE product_id=?`,
     [productId]
   );
   return rows;
 }
 export async function getColors(productId) {
   const [rows] = await pool.query(
-    `select * from product_colors WHERE product_id=?`,
+    `select c.color from product_colors pc LEFT JOIN color c ON c.id = pc.color_id  WHERE product_id=?`,
     [productId]
   );
   return rows;
