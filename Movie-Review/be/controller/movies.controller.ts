3db4b8b33c901b74569b707b6ae6c5a2744d795d
Usukhusk <Usukhuumz12@gmail.com>
2023-04-10 00:15:55 +0800

    need to make a footer

--- Movie-Review/be/controller/movies.controller.ts
+++ Movie-Review/be/controller/movies.controller.ts
@@ -2,11 +2,15 @@ import movieModel from "../models/movies.model";
 import { Request, Response } from "express";
 
 export const getMovies = async (req: Request, res: Response) => {
-  // const page: number = Number(req.query.page);
-  // const moviesPerPage: number = Number(req.query.moviesPerPage);
+  const page: number = Number(req.query.page) || 0;
+  const moviesPerPage: number = Number(req.query.moviesPerPage) || 32;
   try {
-    const movies = await movieModel.find({}).limit(20);
-    // .skip(moviesPerPage * page);
+    const movies = await movieModel
+      .find()
+      .limit(moviesPerPage)
+      .skip(moviesPerPage * page)
+      .sort({ year: -1 });
+
     res.status(200).json(movies);
   } catch (error) {
     res.status(404).json({ data: [] });
