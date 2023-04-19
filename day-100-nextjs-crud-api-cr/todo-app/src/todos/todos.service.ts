aaa671a402b3e1bc32a3013899c02ec1fd395e72
Morning <morning@admins-iMac-3.local>
2023-04-19 09:57:16 +0800

    day-100 yaaaaaaay

---
+++ day-100-nextjs-crud-api-cr/todo-app/src/todos/todos.service.ts
@@ -0,0 +1,38 @@
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { CreateTodoDto } from './dto/create-todo.dto';
+import { UpdateTodoDto } from './dto/update-todo.dto';
+import { InjectModel } from '@nestjs/mongoose';
+import { Todo } from '../models/todo.model';
+import { Model } from 'mongoose';
+@Injectable()
+export class TodosService {
+  constructor(@InjectModel('Todo') private readonly todoModel: Model<Todo>) {}
+
+  async create(createTodoDto: CreateTodoDto): Promise<Todo> {
+    if (createTodoDto.title === '') {
+      throw new BadRequestException('Title is required');
+    }
+    const newTodo = new this.todoModel(createTodoDto);
+    const result = newTodo.save();
+    return result;
+  }
+
+  async findAll() {
+    const result = await this.todoModel.find({});
+    return result;
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} todo`;
+  }
+
+  async update(id: string, updateTodoDto: UpdateTodoDto) {
+    const result = await this.todoModel.findByIdAndUpdate({ _id: id });
+    return result;
+  }
+
+  async remove(id: string) {
+    const result = await this.todoModel.findByIdAndDelete({ _id: id });
+    return result;
+  }
+}
